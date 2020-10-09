e8253e8c52f765edbbc865fcdfe86033455f974e
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-10-09 03:04:55 -0300

    Change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'this-is-me',
+    title: 'This is me',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
