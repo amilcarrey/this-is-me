3c270e91d1d4a0dfeba06bfd42131af4da831fa5
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-10-11 03:20:14 -0300

    Adding about component

---
+++ components/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="text-gray-700 body-font">
+    <div
+      class="container mx-auto flex px-5 py-24 items-center justify-center flex-col"
+    >
+      <div class="text-center lg:w-2/3 w-full">
+        <h1
+          class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
+        >
+          About me
+        </h1>
+        <p class="mb-8 leading-relaxed">
+          I'm a 25-year-old systems engineer and passionate about technology,
+          based in Buenos Aires. Not only I have experience in development and
+          design systems but also in cloud computing, a little bit of BI and
+          project management. I don't like the feeling of not knowing something.
+          However, I like to use this feeling as a motor to continue growing
+          professionally, learning new things, and improve day by day.
+        </p>        
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped></style>
